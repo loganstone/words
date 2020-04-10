00c85163ace9b1982507c10907f5a18e351c8700
Logan <hs.lee.logan@gmail.com>
2020-04-10 16:35:58 +0900

    Add main function

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/loganstone/words
+
+go 1.14
+
+require (
+	github.com/olekukonko/tablewriter v0.0.4 // indirect
+	github.com/ssor/bom v0.0.0-20170718123548-6386211fdfcf // indirect
+	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e // indirect
+	jaytaylor.com/html2text v0.0.0-20200220170450-61d9dc4d7195
+)
---
+++ main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"container/heap"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"regexp"
+	"strings"
+
+	"jaytaylor.com/html2text"
+)
+
+const (
+	pattern = "\\"
+)
+
+var isWord = regexp.MustCompile(`^[[:alpha:]]+$`).MatchString
+
+// Words .
+type Words []*word
+
+func (w Words) Len() int {
+	return len(w)
+}
+
+func (w Words) Less(i, j int) bool {
+	return w[i].num > w[j].num
+}
+
+func (w Words) Swap(i, j int) {
+	w[i], w[j] = w[j], w[i]
+}
+
+// Push .
+func (w *Words) Push(x interface{}) {
+	*w = append(*w, x.(*word))
+}
+
+// Pop .
+func (w *Words) Pop() interface{} {
+	old := *w
+	n := len(old)
+	element := old[n-1]
+	*w = old[0 : n-1]
+	return element
+}
+
+type word struct {
+	txt string
+	num int
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Printf("Usage: %s <html page url>\n", os.Args[0])
+		return
+	}
+
+	url := os.Args[1]
+
+	res, err := http.Get(url)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	page, err := html2text.FromReader(res.Body, html2text.Options{})
+	if err != nil {
+		panic(err)
+	}
+
+	data := make(map[string]int)
+	lines := strings.Split(page, "\n")
+	for _, line := range lines {
+		for _, datum := range strings.Split(line, " ") {
+			w := strings.ToLower(datum)
+			if !isWord(w) {
+				continue
+			}
+			if _, ok := data[w]; ok {
+				data[w]++
+			} else {
+				data[w] = 1
+			}
+		}
+	}
+
+	words := &Words{}
+	heap.Init(words)
+
+	for txt, number := range data {
+		heap.Push(words, &word{txt, number})
+	}
+
+	for words.Len() > 0 {
+		w, ok := heap.Pop(words).(*word)
+		if ok {
+			fmt.Printf("%s, %d\n", w.txt, w.num)
+		}
+	}
+}
